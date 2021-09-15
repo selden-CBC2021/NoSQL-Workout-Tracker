760f6efce183208d9396068e05a27789cc26d39a
Drew Selden <seldend1@gmail.com>
2021-09-15 17:31:04 -0400

    server js working on local now

--- server.js
+++ server.js
@@ -19,9 +19,7 @@ mongoose.connect(
     process.env.MONGODB_URI || 'mongodb://localhost/workout-tracker',
     {
       useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false
+      useUnifiedTopology: true
     }
   );
 
