47299a283ab00bab2541694428665e4b2199b32b
Drew Selden <seldend1@gmail.com>
2021-09-15 16:16:32 -0400

    set up server.js w/ mongodb parameters

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(morgan("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/workout-tracker',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
+
+  app.listen(PORT, () => {
+    console.log(`App running on localhost:${PORT} !`);
+  });
