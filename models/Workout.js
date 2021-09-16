7f91d1de08b537b2bb09742563621602e118fe40
Drew Selden <seldend1@gmail.com>
2021-09-16 12:39:40 -0400

    workout schema

---
+++ models/Workout.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema ({
+    date: {
+        type: Date,
+        default: Date.now
+    }, 
+    exercise: [{
+        type: {
+            type: String,
+            trim: true,
+            required: "select type of workout",
+
+        }, 
+        name: {
+            type: String,
+            trim: true,
+        },
+        duration: {
+            type: Number,
+        },
+        weight: {
+            type: Number,
+        },
+        reps: {
+            type: Number,
+        },
+        sets: {
+            type: Number,
+        }
+        }]
+    
+})
+    
+
+
+
+      
