40acad99394441e795e1b544042b6475cf67ecd1
Drew Selden <seldend1@gmail.com>
2021-09-16 20:11:21 -0400

    deploy

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const Workout = require('../models/workout');
+const Workout = require('../models/workout.js');
 require ('dotenv').config();
 
 mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/workout", {
